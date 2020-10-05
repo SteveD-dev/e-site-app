0937a4011575bdb397a3c01f6c82cc1dca2e782a
djoube steve <sdjoube@gmail.com>
2020-10-06 00:24:21 +0200

    added addItem and removeItem fonctionality to checkout item component

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -10,3 +10,18 @@ export const addItem = item => (
         payload: item
     }
 )
+
+export const clearItemFromCart = item => (
+    {
+        type:cartType.CLEAR_ITEM_FROM_CART,
+        payload: item
+    }
+);
+
+
+export const removeItem = item => (
+    {
+        type:cartType.REMOVE_ITEM,
+        payload: item
+    }
+);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,14 +2,28 @@
 // utility functions allow us to keep our files clean and organize functions that we may need in multiple files in one location 
 
 
-export const addItemToCart = (existingCartItem, addedCartItem) => {
-    const newExistingCart = existingCartItem.find(el => el.id === addedCartItem.id);
+export const addItemToCart = (cartItems, cartToAdd) => {
+    const newCartItem = cartItems.find(el => el.id === cartToAdd.id);
 
-    if(newExistingCart) {
-        return existingCartItem.map(el => el.id === addedCartItem.id  ? 
+    if(newCartItem) {
+        return cartItems.map(el => el.id === cartToAdd.id  ? 
             { ...el, quantity: el.quantity + 1  } 
             : el)
     }
 
-    return  [ ...existingCartItem, {...addedCartItem, quantity: 1}]
+    return  [ ...cartItems, {...cartToAdd, quantity: 1}]
+};
+
+
+
+
+
+export const removeItemFromCart = (cartItems , cartItemToRemove) => {
+    const removeItem = cartItems.find(el => el.id === cartItemToRemove.id);
+
+    if(removeItem.quantity === 1){
+        return cartItems.filter(el => el.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(el => el.id === cartItemToRemove.id ? {...el, quantity: el.quantity - 1} : el)
 }
