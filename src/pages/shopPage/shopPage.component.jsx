9a361f86b5ff8bff1b231b6610c0bb1779f3ab28
djoube steve <sdjoube@gmail.com>
2020-10-16 22:30:33 +0200

    add collection path and component in shop page

--- src/pages/shopPage/shopPage.component.jsx
+++ src/pages/shopPage/shopPage.component.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Route} from 'react-router-dom';
 import { createStructuredSelector} from 'reselect';
 import { selectCollections} from '../../redux/shop/shop.selector';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
 
 
-const ShopPage = ({collections}) => ( 
-        <div className="shop-page">
-            <CollectionOverview />
-        </div>)
+const ShopPage = ({match}) => { 
+    console.log(match);
+        return (<div className="shop-page">
+            <Route exact path={`${match.path}`} component={CollectionOverview} />
+            <Route exact path={`${match.path}/:collectionId`} component={CollectionPage}  />
+        </div>)}
 
 
-const mapStateToProps = createStructuredSelector ({
-collections: selectCollections
-})
+// const mapStateToProps = createStructuredSelector ({
+// collections: selectCollections
+// })
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
